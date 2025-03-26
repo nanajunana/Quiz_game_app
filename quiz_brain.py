50ea85410c3d408bfcb9fb9de7d3a5ffc2a56638
NJN <160208242+nanajunana@users.noreply.github.com>
2025-03-26 18:34:20 +0800

    Update quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -3,14 +3,17 @@ class Quizbrain:
         self.question_list = question_list
         self.question_number = 0
         self.score = 0
-
+        
     def next_question(self):
-        current_question = self.question_list[self.question_number].text
-        current_answer = self.question_list[self.question_number].answer
-        player_answer = input(f"Q.{self.question_number+1}: {current_question} (True/False)?")
-        if player_answer == current_answer:
-            self.score += 1
-            self.question_number += 1
-            print(self.score)
-        else:
-            self.question_number += 1
+        while self.question_number < len(self.question_list) :
+            current_question = self.question_list[self.question_number].text
+            current_answer = self.question_list[self.question_number].answer
+            player_answer = input(f"Q.{self.question_number+1}: {current_question} (True/False)?")
+            if player_answer == current_answer:
+                self.score += 1
+                self.question_number += 1
+                print(self.score)
+            else:
+                self.question_number += 1
+                print(self.score)
+        print(f"You finished the quiz! Your score is {self.score}")
